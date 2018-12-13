5b4dd3c50dd2d6d4d2be6e955ccde1ea2a323ef0
Serhii Kovalenko <serhii.kovalenko@otakoyi.com>
2018-12-13 23:42:53 +0200

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>home</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet ducimus harum atque beatae nesciunt incidunt. Ad debitis ducimus cum error, reiciendis unde ipsa illo harum, maiores ex eius repudiandae, necessitatibus quibusdam rem. Aliquid ipsa libero commodi et officia pariatur exercitationem.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/ViewProfile.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>Profile for</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'ViewProfile',
+		data() {
+			return {
+				userId: this.$route.params.user_id,
+			}
+		}
+	}
+</script>
