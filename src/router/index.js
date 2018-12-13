595b0f8e9b420cd5b8ffbdde4817c155d7b939d9
Serhii Kovalenko <serhii.kovalenko@otakoyi.com>
2018-12-14 01:38:19 +0200

    navbar component

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+// import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/home',
-      name: 'Home',
-      component: Home
-    },
   ]
 })
